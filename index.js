c33315af03d2d7a5f55b32ecb75fdec2c04780c2
Ayyaz Zafar <ayyaz.zaffar@gmail.com>
2020-03-29 22:33:50 +0500

    Created endpoint to get todo list as json

--- index.js
+++ index.js
@@ -3,5 +3,26 @@ const app = express()
 const port = 3000
 
 app.get('/', (req, res) => res.send('<h1>Hello Awais!</h1>'))
+app.get('/work', (req, res) => res.send('<h1>Hello Work!</h1>'))
+app.get('/get_todo_list', (req, res) => {
+
+    let data = [
+        {
+            data: 'Pick milk from shop.', date_time: '10 minutes ago'
+        },
+        {
+            data: 'Go to school.', date_time: '2 minutes ago'
+        },
+        {
+            data: 'Go to the gym.', date_time: '1 minutes ago'
+        }
+
+    ];
+
+    res.send(data);
+
+
+
+})
 
 app.listen(port, () => console.log(`Example app listening on port ${port}!`))
